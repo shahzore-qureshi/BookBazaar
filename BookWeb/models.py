fe44bc5a712a7aba82bac9d7148d058d9726d4ce
Glen Oakley <goakley123@gmail.com>
2013-11-21 12:41:13 -0500

    Added Book and Course models to BookWeb

--- BookWeb/models.py
+++ BookWeb/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    subject = models.CharField(max_length=4)
+    number = models.IntegerField()
+
+class Book(models.Model):
+    CONDITIONS = (
+        ('N','NEW'),
+        ('G','GOOD'),
+        ('O','OKAY'),
+        ('P','POOR'),
+    )
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length = 128)
+    isbn = models.CharField(max_length=10)
+    condition = models.CharField(max_length=16, choices=CONDITIONS)
+    price = models.DecimalField(max_digits=19, decimal_places=4)
+    course = models.ForeignKey(Course)
