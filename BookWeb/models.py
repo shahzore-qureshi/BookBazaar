04504d430aeb4786cd4284e964f8fc47f7bfb1f9
Glen Oakley <goakley123@gmail.com>
2013-11-21 13:36:27 -0500

    Linked books to people

--- BookWeb/models.py
+++ BookWeb/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Person(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    def __str__(self):
+        return "%s %s (%s)" % (self.firstname, self.lastname, self.email)
+
 class Course(models.Model):
     subject = models.CharField(max_length=4)
     number = models.IntegerField()
@@ -21,12 +28,6 @@ class Book(models.Model):
     condition = models.CharField(max_length=16, choices=CONDITIONS)
     price = models.DecimalField(max_digits=19, decimal_places=4)
     course = models.ForeignKey(Course)
+    seller = models.ForeignKey(Person)
     def __str__(self):
         return "%s: %s" % (self.author, self.title)
-
-class Person(models.Model):
-    firstname = models.CharField(max_length=20)
-    lastname = models.CharField(max_length=20)
-    email = models.EmailField()
-    def __str__(self):
-        return "%s %s (%s)" % (self.firstname, self.lastname, self.email)
