b9c022fbc156a1125e9105a0697ae14ae58d52f2
Glen Oakley <goakley123@gmail.com>
2013-11-21 13:03:39 -0500

    Fixed syntax bug in model and added string representations

--- BookWeb/models.py
+++ BookWeb/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Course(models.Model):
     subject = models.CharField(max_length=4)
     number = models.IntegerField()
+    def __str__(self):
+        return "%s%i" % (self.subject, self.number)
 
 class Book(models.Model):
     CONDITIONS = (
@@ -19,12 +21,12 @@ class Book(models.Model):
     condition = models.CharField(max_length=16, choices=CONDITIONS)
     price = models.DecimalField(max_digits=19, decimal_places=4)
     course = models.ForeignKey(Course)
-    
+    def __str__(self):
+        return "%s: %s" % (self.author, self.title)
+
 class Person(models.Model):
-	firstname = models.CharField(max_length=20)
-	lastname = models.CharField(max_length=20)
-	email = models.EmailField([max_length=75, **options])
-	
-class BookSellers(models.Model):
-    seller = models.ForeignKey(Person)
-    bookid = models.ForeignKey(Book)
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    email = models.EmailField()
+    def __str__(self):
+        return "%s %s (%s)" % (self.firstname, self.lastname, self.email)
