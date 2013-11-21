21f198919f1dec4bade82f11393bc3580dc7c78a
derricp1 <derricp1@apps.tcnj.edu>
2013-11-21 12:46:47 -0500

    Update models.py
    Adding book-person linking table

--- BookWeb/models.py
+++ BookWeb/models.py
@@ -19,3 +19,12 @@ class Book(models.Model):
     condition = models.CharField(max_length=16, choices=CONDITIONS)
     price = models.DecimalField(max_digits=19, decimal_places=4)
     course = models.ForeignKey(Course)
+    
+class Person(models.Model):
+	firstname = models.CharField(max_length=20)
+	lastname = models.CharField(max_length=20)
+	email = models.EmailField([max_length=75, **options])
+	
+class BookSellers(models.Model):
+    seller = models.ForeignKey(Person)
+    bookid = models.ForeignKey(Book)
